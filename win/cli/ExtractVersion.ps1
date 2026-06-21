d92df6991a6ccc137937125e8775d7072a4aa881
Ivan Sergeev <ivan.n.sergeev@gmail.com>
2026-06-21 13:21:19 +0500

    Add version for build

---
+++ win/cli/ExtractVersion.ps1
@@ -0,0 +1,16 @@
+$versionFile = Join-Path $PSScriptRoot "version.h"
+$outputFile = Join-Path $PSScriptRoot "version.props"
+
+$content = Get-Content $versionFile -Raw
+if ($content -match 'PEDAL_BUTTONS_VERSION\s+"([^"]+)"') {
+    $version = $Matches[1]
+    $props = @"
+<?xml version="1.0" encoding="utf-8"?>
+<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <PropertyGroup>
+    <AppVersion>$version</AppVersion>
+  </PropertyGroup>
+</Project>
+"@
+    Set-Content -Path $outputFile -Value $props -Encoding UTF8
+}
---
+++ win/cli/version.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define PEDAL_BUTTONS_VERSION "1.0.1"
